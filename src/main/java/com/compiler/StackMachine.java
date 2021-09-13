e426cd72d6081eeb3314691d68e945e15162c8c5
Kiki393 <ashun393@gmail.com>
2021-09-13 00:28:19 +0000

    1.2

--- src/main/java/com/compiler/StackMachine.java
+++ src/main/java/com/compiler/StackMachine.java
@@ -5,24 +5,31 @@ public class StackMachine<E> {
     static int value;
     static String input;
 
+    public static Scanner keyboard = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
+        
 
         // Instance of the stack class
         var stc = new StackCommands<Integer>();
 
         System.out.println("Welcome to the Stack Machine.");
-        System.out.println("Enter 'x' to Stop adding to the stack.");
-        System.out.println("\nEnter a value to add to the stack machine.");
+        System.out.println("Commands: \n1) Enter 'x' to stop \n2) Enter 'd' to display the contents of the stack" + 
+        "\n3) Enter '+' to sum the first two integer values \n4) Enter 's' to swap the first two integer values \n5) Enter 'e' to evaluate the stack");
+        System.out.println("\nEnter values to add to the stack machine.");
 
         do{
+            System.out.print("> ");
             input = keyboard.nextLine();
 
-            if (input.equals("+")) {
-            
+            if (Character.isDigit(input.charAt(0))) { // Checks if input from user is a digit or not
+                value = Integer.parseInt(input);
+                stc.push(value);
+            }
+            else if (input.equals("+")){
                 if (!stc.isEmpty()) {
                     int tempNum1 = stc.pop();
                     int tempNum2 = stc.pop();
@@ -32,24 +39,40 @@ public class StackMachine<E> {
                 }  
                 else{
                     System.out.println("The stack is empty");
-                    return;
                 }
 
+                // Displays stack after summing and pushing result
                 System.out.println();
                 stc.display();
+            }   
+            else if (input.equals("d")) {
+                    System.out.println("\nThe contents of the stack is:");
+                    stc.display();
+                    System.out.println();
+            }
+            else if (input.equals("s")) {
+                if (!stc.isEmpty()) {
+                    int tempNum1 = stc.pop();
+                    int tempNum2 = stc.pop();
 
+                    stc.push(tempNum2);
+                    stc.push(tempNum1);
+                }  
+                else{
+                    System.out.println("The stack is empty");
+                }
             }
-            else if (Character.isDigit(input.charAt(0))) {
-                value = Integer.parseInt(input);
-                stc.push(value);
+            else if (input.equals("e")) {
+                int sum = 0;
+                for (int i = 0; i <= stc.size; i++) {
+                    sum = sum + stc.pop();
+                }
+                System.out.println("\nThe total sum of values in the stack is: " + sum);
             }
-            
-            
         }while(!input.equals("x"));
         
-        System.out.println("\nExited");
-        stc.display();
-
+        System.out.println("\nStack exited");
+        
         keyboard.close();
     }
 }
