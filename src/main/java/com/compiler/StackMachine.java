cdea92830b18404adb69c94d3c0111c2cdc34e69
Kiki393 <ashun393@gmail.com>
2021-09-22 01:08:20 +0000

    .

--- src/main/java/com/compiler/StackMachine.java
+++ src/main/java/com/compiler/StackMachine.java
@@ -14,7 +14,7 @@ public class StackMachine<E> {
         // Instance of the stack class
         var stc = new StackCommands<Integer>();
 
-        System.out.println("Welcome to the Stack Machine.");
+        System.out.println("\nWelcome to the Stack Machine.");
         System.out.println("Commands: \n1) Enter 'x' to stop. \n2) Enter 'd' to display the contents of the stack." + 
         "\n3) Enter '+' to sum the first two integer values. \n4) Enter 's' to swap the first two integer values. \n5) Enter 'e' to evaluate the stack." +
         "NOTE: This will remove all elements from the stack and sum them.");
